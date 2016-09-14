c577efff550db74aa959451d8d8dec73011869db
ivs5069 <ivs5069@gmail.com>
2016-09-13 21:43:15 -0400

    Changed if statement for user input to switch case statements

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -11,96 +11,82 @@ package wheeloffortune;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class WheelOfFortune {
 
-	//method to print out instructions
-	public static void displayInstructions(){
+    //method to print out instructions
+    public static void displayInstructions() {
 
-		System.out.println("Please select an option by entering the number:"
-				+ "\n1. Spin the Wheel"
-				+ "\n2. Buy a Vowel"
-				+ "\n3. Solve the Puzzle"
-				+ "\n4. Quit the Game");
+        System.out.println("Please select an option by entering the number:"
+                + "\n1. Spin the Wheel"
+                + "\n2. Buy a Vowel"
+                + "\n3. Solve the Puzzle"
+                + "\n4. Quit the Game");
 
-	}
-	//method to get users input
-	public static int getUserInput(){
-		int userInp = 0;
-		Scanner numbInp = new Scanner(System.in); //Scanner object for ints and floats
-		boolean run = true;
+    }
+    //method to get users input
 
-		//If the loop manages to complete, the user entered a valid input
-		while(run == true){
-			//Display to the user the instructions
-			displayInstructions();
+    public static int getUserInput() {
+        int userInp = 0;
+        Scanner numbInp = new Scanner(System.in); //Scanner object for ints and floats
+        boolean run = true;
 
-			//Exception handling if the user inputs something other than an int
-			try{
-				userInp = numbInp.nextInt();
+        //If the loop manages to complete, the user entered a valid input
+        while (run == true) {
+            //Display to the user the instructions
+            displayInstructions();
 
+            //Exception handling if the user inputs something other than an int
+            try {
+                userInp = numbInp.nextInt();
 
-				//input validation to make sure user selects a valid int
-				while(userInp < 1 || userInp > 4)
-				{
-					System.out.println("Invalid input, pick a number 1 through 4.");
-					displayInstructions();
-					userInp = numbInp.nextInt();
-				}
-			}
-			catch(InputMismatchException E){
-				System.out.println("Invalid input, pick a number 1 through 4.");
-				//Clear out the Scanner, otherwise it will infinitly loop
-				numbInp.next();
-				//Return to the top of the while loop
-				continue;
-			}
-			run = false;
-		}
+                //input validation to make sure user selects a valid int
+                while (userInp < 1 || userInp > 4) {
+                    System.out.println("Invalid input, pick a number 1 through 4.");
+                    displayInstructions();
+                    userInp = numbInp.nextInt();
+                }
+            } catch (InputMismatchException E) {
+                System.out.println("Invalid input, pick a number 1 through 4.");
+                //Clear out the Scanner, otherwise it will infinitly loop
+                numbInp.next();
+                //Return to the top of the while loop
+                continue;
+            }
+            run = false;
+        }
 
-		return userInp;
-	}
+        return userInp;
+    }
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-
-		//Get the users input and assign it to userInput variable
-
-
-
-		while (true){
-
-			//Get the users input and assign it to userInput variable    
-			int userInput = getUserInput();
-
-
-			//If user selects spin wheel
-			if(userInput == 1){
-				System.out.println("You have selected: Spin the Wheel.");
-				continue;
-			}
-
-			//if user selects buy a vowel
-			else if(userInput == 2){
-				System.out.println("You have selected: Buy a Vowel.");
-			}
-
-			//if user selects solve puzzle
-			else if(userInput == 3) {
-				System.out.println("You have selected: Solve the Puzzle.");
-			}
-
-			else if(userInput == 4) {
-				System.out.println("You have selected: Quit the Game.");
-				System.out.println("Quitting the game");
-				break;
-			}
-		}
-	}
-
-
+        OUTER:
+        while (true) {
+            int userInput = getUserInput();
+            //If user selects spin wheel
+            switch (userInput) {
+            //if user selects buy a vowel
+                case 1:
+                    System.out.println("You have selected: Spin the Wheel.");
+                    break;
+            //if user selects solve puzzle
+                case 2:
+                    System.out.println("You have selected: Buy a Vowel.");
+                    break;
+                case 3:
+                    System.out.println("You have selected: Solve the Puzzle.");
+                    break;
+                case 4:
+                    System.out.println("You have selected: Quit the Game.");
+                    System.out.println("Quitting the game");
+                    break OUTER;
+                default:
+                    break;
+            }
+        }
+    }
 
 }
